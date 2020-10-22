09bf4f472dc530b4f68f1016db68dfabc4c4c9eb
Matt Chang <mattchangnyc@gmail.com>
2020-10-22 16:51:43 -0400

    Updates

---
+++ precache-manifest.7f717ca78a4701acf4be2d65e4a96712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1659de31ebb2728b9c62395625046fa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b4c920fec4d9f42302a",
+    "url": "/monsters-rolodex/static/css/main.41355046.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4c920fec4d9f42302a",
+    "url": "/monsters-rolodex/static/js/main.afe53629.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
